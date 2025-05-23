02d74c02b88046d74992946788f65cb014da5b44
jolade okunlade <jokunlade@gmail.com>
2025-05-23 03:40:51 +0100

    feat:user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let mut x: String = String::new();
+    let mut y: String = String::new();
+
+    println!("Hello, Welcome to Jolah's rust calculator!");
+
+    println!("Please enter the first number: ");
+    io::stdin().read_line(&mut x).expect("Invalid input");
+
+    println!("Please enter the second number: ");
+    io::stdin().read_line(&mut y).expect("Invalid input");
 }
