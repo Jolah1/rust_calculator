85a2cc63de665dfb06b4fade78279a0050dd4c15
jolade okunlade <jokunlade@gmail.com>
2025-05-23 06:32:49 +0100

    calculator added in a loop

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,71 @@
 use std::io;
 fn main() {
-    let result: f64;
+    let mut result: f64;
+    let mut y_or_n: String = String::new();
+    let ops: [fn(f64, f64) -> f64; 5] = [add, subtract, multiply, divide, mod_];
 
-    println!("Hello, Welcome to Jolah's rust calculator!");
+    loop {
+        println!("Hello, Welcome to Jolah's rust calculator!");
 
-    println!("Please enter the first number: ");
-    let x: f64 = input_parser();
+        println!("Please enter the first number: ");
+        let x: f64 = input_parser();
 
-    if f64::is_nan(x) {
-        println!("Invalid input!");
-        return;
-    }
+        if f64::is_nan(x) {
+            println!("Invalid input!");
+            return;
+        }
 
-    println!("Please enter the second number: ");
-    let y: f64 = input_parser();
+        println!("Please enter the second number: ");
+        let y: f64 = input_parser();
 
-    if f64::is_nan(y) {
-        println!("Invalid input!");
-        return;
-    }
-    println!("Please select the operation you want to perform:");
+        if f64::is_nan(y) {
+            println!("Invalid input!");
+            return;
+        }
 
-    println!("List of operators:");
-    println!("(1) Add");
-    println!("(2) Subtract");
-    println!("(3) Multiply");
-    println!("(4) Divide");
-    println!("(5) Modulus");
-    println!("Select the number associated with the desired operation: ");
+        println!("List of operators:");
+        println!("(1) Add");
+        println!("(2) Subtract");
+        println!("(3) Multiply");
+        println!("(4) Divide");
+        println!("(5) Modulus");
+        println!("Select the number associated with the desired operation: ");
 
-    let op: f64 = input_parser();
+        let op: f64 = input_parser();
 
-    if f64::is_nan(op) {
-        println!("Invalid input!");
-        return;
-    }
+        if f64::is_nan(op) {
+            println!("Invalid input!");
+            return;
+        }
 
-    let op: i32 = op as i32;
+        let op: i32 = op as i32;
 
-    match op {
-        1 => result = x + y,
-        2 => result = x - y,
-        3 => result = x * y,
-        4 => result = x / y,
-        5 => result = x % y,
-        _ => {
+        if op > 5 || op < 1 {
             println!("Error: Invalid selection!");
             return;
         }
-    }
 
-    println!("The result is: {}", result);
-    println!("Thank you for using Jolah's calculator!");
-    println!("Have a great day!");
+        result = ops[(op - 1) as usize](x, y);
+
+        println!("The result is: {}", result);
+        println!("continue? (y/n): ");
+        io::stdin().read_line(&mut y_or_n).expect("Invalid Input");
+
+        if y_or_n.trim() == "n" {
+            println!("Thank you for using Jolah's calculator!");
+            println!("Have a great day!");
+            break;
+        } else if y_or_n.trim() != "y" {
+            println!("Invalid input!");
+            println!("Thank you for using Jolah's calculator!");
+            println!("Have a great day!");
+            break;
+        } else {
+            y_or_n.clear();
+        }
+    }
 }
+
 fn input_parser() -> f64 {
     let mut x = String::new();
     io::stdin().read_line(&mut x).expect("Invalid Input");
@@ -63,5 +75,26 @@ fn input_parser() -> f64 {
             return f64::NAN;
         }
     };
+
     return x;
+}
+
+fn add(x: f64, y: f64) -> f64 {
+    x + y
+}
+
+fn subtract(x: f64, y: f64) -> f64 {
+    x - y
+}
+
+fn multiply(x: f64, y: f64) -> f64 {
+    x * y
+}
+
+fn divide(x: f64, y: f64) -> f64 {
+    x / y
+}
+
+fn mod_(x: f64, y: f64) -> f64 {
+    x % y
 }
